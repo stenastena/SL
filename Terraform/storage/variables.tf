a4a5767aa685567c516693ea1c589425bc6ca23c
Serg Osh <stolb@bk.ru>
2020-10-07 16:39:04 +0300

    Storage Terraform script

---
+++ Terraform/storage/variables.tf
@@ -0,0 +1,18 @@
+variable "prefix" {
+  description   = "The prefix which should be used for all resources in this example"
+  default       = "osh"
+}
+
+variable "location" {
+  description   = "The Azure Region in which all resources in this example should be created."
+  default       = "West Europe"
+}
+
+variable "tags" {
+  type          = map
+
+  default = {
+    Project     = "Leche Dos Pinos"
+    Dept        = "Engineering"
+  }
+}
