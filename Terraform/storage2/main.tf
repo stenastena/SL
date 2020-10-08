49b1ed2bd9339c9c44f4c26985fdb9d908404a94
Serg Osh <stolb@bk.ru>
2020-10-08 09:00:27 +0300

    Something not important

--- Terraform/storage2/main.tf
+++ Terraform/storage2/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_storage_account" "sta" {
   name                     = "${var.prefix}storageacct"
   resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.example.location
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   allow_blob_public_access = "true"
